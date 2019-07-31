35a01542697d9fd668af9916528f4efdbdea584b
Osemeke Nwaka <o.ch.nwaka@gmail.com>
2019-07-31 09:54:31 -0400

    Created final exercises

--- chinook/admin.py
+++ chinook/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from chinook.models import Artist, Album, MediaType, Genre, Track, Playlist     
 
-# Register your models here.
+# Register your models here
+admin.site.register(Artist)
+admin.site.register(Album)
+admin.site.register(MediaType)
+admin.site.register(Genre)
+admin.site.register(Track)
+admin.site.register(Playlist)
